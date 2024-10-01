7dcb6b86050680b22cd23996f421d03f5aeefb71
Joe <joe_ethan2@hotmail.com>
2024-10-01 16:47:56 +0100

    add: AuthContext now has username available within full app context
    Co-authored-by: Ben McCarthy <Dimeben@users.noreply.github.com>

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,46 +1,66 @@
 import React, { createContext, useState, ReactNode, useEffect } from "react";
-import { firebase, auth } from "../firebase";
+import { firebase, auth, db } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 interface AuthContextType {
   isAuthenticated: boolean;
   userId: string | null;
+  username: string | null;
 }
 
 export const AuthContext = createContext<AuthContextType>({
   isAuthenticated: false,
   userId: null,
+  username: null,
 });
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
+const fetchUsername = async (uid: string): Promise<string | null> => {
+  const userDoc = doc(db, "users", uid);
+  const userGrab = await getDoc(userDoc);
+
+  if (userGrab.exists()) {
+    return userGrab.data()?.username || null;
+  }
+
+  const djDoc = doc(db, "djs", uid);
+  const djGrab = await getDoc(djDoc);
+
+  if (djGrab.exists()) {
+    return djGrab.data()?.username || null;
+  }
+
+  console.log("Document does not exist for UID", uid);
+  return null;
+};
+
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [userId, setUserId] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log(
-        "User State Changed:",
-        user?.uid,
-        "Authenticated?",
-        isAuthenticated
-      ); // debugging
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         setIsAuthenticated(true);
         setUserId(user.uid);
+        const fetchedUsername = await fetchUsername(user.uid);
+        setUsername(fetchedUsername);
       } else {
         setIsAuthenticated(false);
         setUserId(null);
+        setUsername(null);
       }
     });
     return () => unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, userId }}>
+    <AuthContext.Provider value={{ isAuthenticated, userId, username }}>
       {children}
     </AuthContext.Provider>
   );
