5d5d79bc6334cc31f1ac2a04ba6a1f9884ef5310
Joe <joe_ethan2@hotmail.com>
2024-10-01 12:24:21 +0100

    feat: Authentication and UserID context added - dynamically changes based on onAuthStateChanged

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useState, ReactNode, useEffect } from "react";
+import { firebase, auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  userId: string | null;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  isAuthenticated: false,
+  userId: null,
+});
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log(
+        "User State Changed:",
+        user.uid,
+        "Authenticated?",
+        isAuthenticated
+      ); // debugging
+      if (user) {
+        setIsAuthenticated(true);
+        setUserId(user.uid);
+      } else {
+        setIsAuthenticated(false);
+        setUserId(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, userId }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
